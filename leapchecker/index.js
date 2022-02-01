d8930f6958eeaa279c0145faef94ce24767cfeb5
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-02-01 21:34:51 +0100

    Update index.js

--- leapchecker/index.js
+++ leapchecker/index.js
@@ -4,16 +4,16 @@ function myFunction() {
     if(leapYear % 4 === 0){
         
         if(leapYear % 400 === 0){
-            document.getElementById("demo").innerHTML = "It is a prime"
+            document.getElementById("demo").innerHTML = "It is a leay year"
         }
 
         else {
-            document.getElementById("demo").innerHTML = "It is not a prime"
+            document.getElementById("demo").innerHTML = "It is not a leap year"
         }
     }
 
     else {
-        document.getElementById("demo").innerHTML = "It is not a prime";
+        document.getElementById("demo").innerHTML = "It is not a leap year";
     }
     
 }
